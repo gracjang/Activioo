f4e5df19b50a8596bd8ee761dbd6485564dfa2e7
Gracjan <graku12@gmail.com>
2020-06-06 17:22:02 +0200

    Added NavBar and hooks

--- src/activioo.web/src/app/layout/App.tsx
+++ src/activioo.web/src/app/layout/App.tsx
@@ -1,42 +1,32 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { Header, Icon, List } from 'semantic-ui-react'
-import { IActivity } from '../models/activity'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Header, Icon, List } from "semantic-ui-react";
+import { IActivity } from "../models/activity";
+import { NavBar } from "../../features/nav/NavBar";
 
-interface IState {
-  activities: IActivity[]
-}
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([])
 
-class App extends Component<{}, IState> {
-  readonly state: IState = {
-    activities: [],
-  }
+  useEffect(() => {
+      axios
+        .get<IActivity[]>("http://localhost:5000/api/activities")
+        .then((response) => {
+          setActivities(response.data)
+        });
+  }, []);
 
-  componentDidMount() {
-    axios.get<IActivity[]>('http://localhost:5000/api/activities')
-      .then((response) => {
-        this.setState({
-          activities: response.data
-        })
-      })
-  }
-
-  render() {
-    return (
-      <div>
-        <Header as='h2'>
-          <Icon name='users' />
-          <Header.Content>Activioo</Header.Content>
-        </Header>
-        <List>
-          {this.state.activities.map((activity) => (
-            <List.Item key={activity.id}>{activity.title} {activity.city}</List.Item>
-          ))}
-        </List>
-      </div>
-    );
-  }
-}
-
+  return (
+    <div>
+      <NavBar /> 
+      <List>
+        {activities.map(activity => (
+          <List.Item key={activity.id}>
+            {activity.title} {activity.city}
+          </List.Item>
+        ))}
+      </List>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/activioo.web/src/features/nav/NavBar.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Segment, Menu, Container, Button } from "semantic-ui-react";
+
+export const NavBar = () => {
+  return (
+    <Menu fixed="top" inverted>
+      <Container>
+        <Menu.Item header>
+          <img src="/assets/logo.png" alt="logo" />
+          Activioo
+        </Menu.Item>
+        <Menu.Item name="Activities" />
+        <Menu.Item>
+          <Button positive content="Create Activity" />
+        </Menu.Item>
+      </Container>
+    </Menu>
+  );
+};
