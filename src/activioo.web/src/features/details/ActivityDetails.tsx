ebab245e915c71ca90301964fcb62eb4acd5972b
Gracjan <graku12@gmail.com>
2020-06-06 21:36:48 +0200

    Added openCreateForm and cancel edit action

--- src/activioo.web/src/features/details/ActivityDetails.tsx
+++ src/activioo.web/src/features/details/ActivityDetails.tsx
@@ -5,10 +5,11 @@ import { IActivity } from './../../app/models/activity';
 
 interface IProps {
   activity: IActivity;
-  setEditMode: (editMode: boolean) => void
+  setEditMode: (editMode: boolean) => void;
+  setSelectedActivity: (activity: IActivity | null) => void;
 }
 
-export const ActivityDetails: React.FC<IProps> = ({activity, setEditMode}) => {
+export const ActivityDetails: React.FC<IProps> = ({activity, setEditMode, setSelectedActivity}) => {
   return (
     <Card fluid>
       <Image src={`/assets/categoryImages/${activity.category}.jpg`} wrapped ui={false} />
@@ -25,7 +26,7 @@ export const ActivityDetails: React.FC<IProps> = ({activity, setEditMode}) => {
         <Button.Group widths={2} size='large'>
           <Button onClick={() => setEditMode(true)} icon='edit' color='blue' content='Edit' />
           <Button.Or />
-          <Button icon='cancel' color='red' content='Cancel' />
+          <Button onClick={() => setSelectedActivity(null)} icon='cancel' color='red' content='Cancel' />
         </Button.Group>
       </Card.Content>
     </Card>
--- src/activioo.web/src/features/form/ActivityForm.tsx
+++ src/activioo.web/src/features/form/ActivityForm.tsx
@@ -1,17 +1,91 @@
-import React from 'react'
-import { Segment, Form } from 'semantic-ui-react';
+import React, { useState } from "react";
+import { Segment, Form, Button } from "semantic-ui-react";
+import { IActivity } from "../../app/models/activity";
 
-export const ActivityForm = () => {
-  return (
-    <Segment>
-      <Form>
-        <Form.Input icon='write' iconPosition='left' placeholder='Title' />
-        <Form.TextArea rows={2} placeholder='Description' />
-        <Form.Input placeholder='Category' />
-        <Form.Input icon='calendar alternate' iconPosition='left' type='date' placeholder='Date' />
-        <Form.Input icon='building' iconPosition='left' placeholder='City' />
-      </Form>
-
-    </Segment>
-  )
+interface IProps {
+  setEditMode: (editMode: boolean) => void;
+  activity: IActivity;
 }
+
+export const ActivityForm: React.FC<IProps> = ({
+  setEditMode,
+  activity: initialFormState,
+}) => {
+  const initializeForm = () => {
+    if (initialFormState) {
+      return initialFormState;
+    } else {
+      return {
+        id: "",
+        title: "",
+        category: "",
+        description: "",
+        date: "",
+        city: "",
+        venue: "",
+      };
+    }
+  };
+
+  const [activity, setActivity] = useState<IActivity>(initializeForm);
+
+  return (
+    <Segment clearing>
+      <Form>
+        <Form.Input
+          icon="write"
+          iconPosition="left"
+          placeholder="Title"
+          value={activity.title}
+        />
+        <Form.TextArea
+          rows={2}
+          placeholder="Description"
+          value={activity.description}
+        />
+        <Form.Input
+          icon="terminal"
+          iconPosition="left"
+          placeholder="Category"
+          value={activity.category}
+        />
+        <Form.Input
+          icon="calendar alternate"
+          iconPosition="left"
+          type="date"
+          placeholder="Date"
+          value={activity.date}
+        />
+        <Form.Input
+          icon="building"
+          iconPosition="left"
+          placeholder="City"
+          value={activity.city}
+        />
+        <Form.Input
+          icon="hdd"
+          iconPosition="left"
+          placeholder="Venue"
+          value={activity.venue}
+        />
+        <Button
+          icon="add"
+          color="green"
+          floated="right"
+          inverted
+          type="submit"
+          content="Submit"
+        />
+        <Button
+          onClick={() => setEditMode(false)}
+          icon="cancel"
+          color="red"
+          floated="right"
+          inverted
+          type="button"
+          content="Cancel"
+        />
+      </Form>
+    </Segment>
+  );
+};
