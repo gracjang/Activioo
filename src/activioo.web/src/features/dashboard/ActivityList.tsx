9ae5ab72cde8d51eab7d83a89ee65f330898c326
Gracjan <graku12@gmail.com>
2020-06-07 13:32:50 +0200

    Added delete button and action for it

--- src/activioo.web/src/features/dashboard/ActivityList.tsx
+++ src/activioo.web/src/features/dashboard/ActivityList.tsx
@@ -5,11 +5,13 @@ import { IActivity } from "../../app/models/activity";
 interface IProps {
   activities: IActivity[];
   selectActivity: (id: string) => void;
+  deleteActivity: (id: string) => void;
 }
 
 export const ActivityList: React.FC<IProps> = ({
   activities,
   selectActivity,
+  deleteActivity
 }) => {
   return (
     <Segment clearing>
@@ -31,6 +33,14 @@ export const ActivityList: React.FC<IProps> = ({
                   floated="right"
                   content="View"
                   color="blue"
+                  inverted
+                />
+                <Button
+                  onClick={() => deleteActivity(activity.id)}
+                  floated="right"
+                  content="Delete"
+                  color="red"
+                  inverted
                 />
                 <Label basic content={activity.category} />
               </Item.Extra>
