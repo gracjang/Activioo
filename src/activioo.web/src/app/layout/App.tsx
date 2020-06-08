a5e80cce4d30b0527f2b7a74286709fe8082051d
Gracjan <graku12@gmail.com>
2020-06-08 20:44:18 +0200

    Fixed button delete target

--- src/activioo.web/src/app/layout/App.tsx
+++ src/activioo.web/src/app/layout/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Fragment } from "react";
+import React, { useState, useEffect, Fragment, SyntheticEvent } from "react";
 import "./styles.css";
 import { Container } from "semantic-ui-react";
 import { IActivity } from "../models/activity";
@@ -15,6 +15,7 @@ const App = () => {
   const [editMode, setEditMode] = useState(false);
   const [loading, setLoading] = useState(true);
   const [submitting, setSubmitting] = useState(false);
+  const [target, setTarget] = useState('');
 
   const handleSelectActivity = (id: string) => {
     setSelectedActivity(activities.filter((x) => x.id === id)[0]);
@@ -47,8 +48,9 @@ const App = () => {
     }).then(() => setSubmitting(false))
   };
 
-  const handleDeleteActivity = (id: string) => {
+  const handleDeleteActivity = (event: SyntheticEvent<HTMLButtonElement> ,id: string) => {
     setSubmitting(true);
+    setTarget(event.currentTarget.name)
     agent.Activities.delete(id).then(() => {
       setActivities([...activities.filter((x) => x.id !== id)]);
     }).then(() => setSubmitting(false))
@@ -83,6 +85,7 @@ const App = () => {
           editActivity={handleEditActivity}
           deleteActivity={handleDeleteActivity}
           submitting={submitting}
+          target={target}
         />
       </Container>
     </Fragment>
