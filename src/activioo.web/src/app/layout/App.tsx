4e97cd96f3f4464912880a32085b640c008d7ab7
Gracjan <graku12@gmail.com>
2020-06-22 21:51:28 +0200

    Added HomePage on other page | Added scrollToTop

--- src/activioo.web/src/app/layout/App.tsx
+++ src/activioo.web/src/app/layout/App.tsx
@@ -3,32 +3,44 @@ import "./styles.css";
 import { Container } from "semantic-ui-react";
 import NavBar from "../../features/nav/NavBar";
 import ActivityDashboard from "../../features/dashboard/ActivityDashboard";
-import { LoadingComponent } from './LoadingComponent';
-import ActivityStore from '../stores/ActivityStore';
-import {observer} from 'mobx-react-lite';
+import { LoadingComponent } from "./LoadingComponent";
+import ActivityStore from "../stores/ActivityStore";
+import { observer } from "mobx-react-lite";
 import { Route, withRouter, RouteComponentProps } from "react-router-dom";
-import { HomePage } from './../../features/home/HomePage';
+import HomePage from "./../../features/home/HomePage";
 import ActivityForm from "../../features/form/ActivityForm";
 import ActivityDetails from "../../features/details/ActivityDetails";
 
-const App: React.FC<RouteComponentProps> = ({location}) => {
-  const activityStore = useContext(ActivityStore)
+const App: React.FC<RouteComponentProps> = ({ location }) => {
+  const activityStore = useContext(ActivityStore);
 
   useEffect(() => {
     activityStore.loadActivities();
   }, [activityStore]);
-  
-  if(activityStore.loadingInitial) return <LoadingComponent content="Loading activities..."/>
+
+  if (activityStore.loadingInitial)
+    return <LoadingComponent content="Loading activities..." />;
 
   return (
     <Fragment>
-      <NavBar/>
-      <Container style={{ marginTop: "7em" }}>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/activities' component={ActivityDashboard} />
-        <Route path='/activities/:id' component={ActivityDetails} />
-        <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
-      </Container>
+      <Route exact path="/" component={HomePage} />
+      <Route
+        path={"/(.+)"}
+        render={() => (
+          <Fragment>
+            <NavBar />
+            <Container style={{ marginTop: "7em" }}>
+              <Route exact path="/activities" component={ActivityDashboard} />
+              <Route path="/activities/:id" component={ActivityDetails} />
+              <Route
+                key={location.key}
+                path={["/createActivity", "/manage/:id"]}
+                component={ActivityForm}
+              />
+            </Container>
+          </Fragment>
+        )}
+      />
     </Fragment>
   );
 };
--- src/activioo.web/src/index.tsx
+++ src/activioo.web/src/index.tsx
@@ -4,9 +4,11 @@ import './app/layout/styles.css';
 import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom'
+import ScrollToTop from './app/layout/ScrollToTop';
 
 ReactDOM.render(
   <BrowserRouter>
+  <ScrollToTop />
     <App />
   </BrowserRouter>,
   document.getElementById('root')
